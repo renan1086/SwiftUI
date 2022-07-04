dcd9d31c8435dbe2646afe80b2b2d6266bf59bc2
Renan R Almeida <renan.almeida@zup.com.br>
2022-07-04 16:55:15 -0300

    Add Avocados

---
+++ 10.Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by ZUP-5876 on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

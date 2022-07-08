bf3d92ea76dc7a9b3975fed99d680ebe37e66b5f
Renan R Almeida <renan.almeida@zup.com.br>
2022-07-08 10:13:55 -0300

    Implemented RipeningStageView

--- 10.Avocados/Avocados/View/RipeningStageView.swift
+++ 10.Avocados/Avocados/View/RipeningStageView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct RipeningStageView: View {
+    
+    var ripeningStages: [Ripening] = ripeningData
+
     var body: some View {
-        Text("Ripening Stage")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .center, spacing: 25) {
+                    ForEach(ripeningStages) { item in
+                        RipeningView(ripening: item)
+                    }
+                }
+                .padding(.vertical)
+                .padding(.horizontal, 25)
+                
+                Spacer()
+            }
+        }
     }
 }
 

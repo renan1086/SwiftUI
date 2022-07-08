1e9b49f198436e85deb02ff8c9ef3208cde0ea31
Renan R Almeida <renan.almeida@zup.com.br>
2022-07-08 10:23:13 -0300

    Add Slot Machine

---
+++ 11.Slot Machine/Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by ZUP-5876 on 08/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

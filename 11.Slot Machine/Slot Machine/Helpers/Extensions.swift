9075c3e2af3cd651ccd1af6e78cfb3e4fae1b62d
Renan R Almeida <renan.almeida@zup.com.br>
2022-07-11 15:08:59 -0300

    update

---
+++ 11.Slot Machine/Slot Machine/Helpers/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  Slot Machine
+//
+//  Created by ZUP-5876 on 11/07/22.
+//
+
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+    
+    func scoreNumberStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.heavy)
+    }
+}

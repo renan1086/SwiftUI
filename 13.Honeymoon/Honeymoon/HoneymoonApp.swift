3fcab2faee14ee5689ef255a6fa5f97a6625f69e
Renan R Almeida <renan.almeida@zup.com.br>
2022-07-13 14:15:44 -0300

    Add HoneymoonApp

---
+++ 13.Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by ZUP-5876 on 13/07/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 13.Honeymoon/Honeymoon/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by ZUP-5876 on 13/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

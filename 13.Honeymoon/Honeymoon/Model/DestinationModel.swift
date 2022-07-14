de5a76c78782902692e3a66d9dbc3eff6b540f53
Renan R Almeida <renan.almeida@zup.com.br>
2022-07-14 11:54:03 -0300

    Update

---
+++ 13.Honeymoon/Honeymoon/Model/DestinationModel.swift
@@ -0,0 +1,14 @@
+//
+//  DestinationModel.swift
+//  Honeymoon
+//
+//  Created by ZUP-5876 on 13/07/22.
+//
+
+import SwiftUI
+
+struct Destination {
+  var place: String
+  var country: String
+  var image: String
+}
